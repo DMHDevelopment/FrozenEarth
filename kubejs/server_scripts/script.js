19d1f9a7fc8cac86452277bb65874303e1ad4dd3
JWTHDYTWA <30724185+Warrock3280@users.noreply.github.com>
2021-01-19 17:54:58 +0700

    Supergalaxy mega gurren COMMIT

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -28,46 +28,6 @@ console.info('Loading Chapter 1 recipes...');
 
 
 events.listen('recipes', event => {
-    event.remove({output: 'exnihilosequentia:sieve'});
-    event.remove({output: 'exnihilosequentia:mesh_string'});
-    event.remove({output: 'exnihilosequentia:mesh_flint'});
-    event.remove({output: 'exnihilosequentia:mesh_iron'});
-    event.shaped(item.of('exnihilosequentia:sieve', 1), [
-        'A A',
-        'BCB',
-        'D D'
-    ], {
-        A: 'naturesaura:ancient_log',
-        B: 'naturesaura:ancient_planks',
-        C: 'naturesaura:ancient_slab',
-        D: 'naturesaura:ancient_stick'
-    });
-    event.shaped(item.of('exnihilosequentia:mesh_string', 1), [
-        'ACA',
-        'CBC',
-        'ACA'
-    ], {
-        A: 'minecraft:string',
-        B: item.of('naturesaura:aura_bottle', {stored_type: "naturesaura:overworld"}),
-        C: item.of(item.of('pickletweaks:mesh', {Damage: 0}))
-    });
-    event.shaped(item.of('exnihilosequentia:mesh_flint', 1), [
-        'AAA',
-        'ACA',
-        'AAA'
-    ], {
-        A: 'minecraft:flint',
-        C: item.of('exnihilosequentia:mesh_string', {Damage: 0})
-    });
-    event.shaped(item.of('exnihilosequentia:mesh_iron', 1), [
-        'ABA',
-        'BCB',
-        'ABA'
-    ], {
-        A: 'create:iron_sheet',
-        B: 'naturesaura:infused_iron',
-        C: item.of('exnihilosequentia:mesh_flint', {Damage: 0})
-    });
 
     // Change receipt for "Iron chests" mod
     event.remove({mod: 'ironchest'});
--- scripts/recipes/chapter_1/tree_stump.zs
+++
@@ -1,8 +0,0 @@
-import crafttweaker.api.item.IIngredient;
-import crafttweaker.api.item.IItemStack;
-import crafttweaker.api.data.StringData;
-
-import mods.stone_age.TreeStumpManager;
-
-<recipetype:stone_age:tree_stump>.addRecipe("sawdust", <item:immersiveengineering:dust_wood>, <item:stone_age:stone_hammer>.withTag({Damage: 0 as int}), <tag:minecraft:wooden_slabs>, 4);
-TreeStumpManager.INSTANCE.addRecipe("sawdust", <item:immersiveengineering:dust_wood>, <item:stone_age:stone_hammer>.withTag({Damage: 0 as int}), <tag:minecraft:wooden_slabs>, 4);
